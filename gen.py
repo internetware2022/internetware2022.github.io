d465512d1175b049979eedf9586e68aeadbc0a20
internetware2022 <93561065+internetware2022@users.noreply.github.com>
2022-05-25 13:19:58 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -16,12 +16,12 @@ sections = [
     ('committee', 'Committee'),
     ('venue', 'Venue and Access'),
     ('travel', 'Travel and Foods'),
-    ('accommodation', 'Accommodation'),
+    ('registration', 'Registration'),
 ]
 
 def get_sections():
     for sec, desc in sections:
-        md_html = markdown.markdown((SRC / f'{sec}.md').read_text())
+        md_html = markdown.markdown((SRC / f'{sec}.md').read_text(encoding='utf-8'))
         yield { 'id': sec, 'html': md_html, 'desc': desc }
 
-Path('index.html').write_text(jinja2.Template(TEMPLATE).render(sections=list(get_sections())))
+Path('index.html').write_text(jinja2.Template(TEMPLATE).render(sections=list(get_sections())),encoding='utf-8')
